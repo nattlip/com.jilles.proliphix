cb07fb7afa5de0d9d5630e8e6f4bdc9c45e312e2
nattlip <nattelip@hotmail.com>
2017-10-22 23:11:01 +0200

    added ;  capabilitiy target_temperature get set , thermostat_mode get

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
  Proliphix NT20 and NT10 thermostats
 
- 22-10-2017   inital commit for github , can only read temperature average which is the local temperature if you have a NT10
+ 22-10-2017   inital commit for github and Athom appstore
+              can  read temperature average which is the local temperature if you have a NT10
+              can read and set threhold temperature 
+			  can read heat state on off 
--- drivers/nt10/device.js
+++ drivers/nt10/device.js
@@ -27,10 +27,18 @@ class NT10Device extends Homey.Device {
         this.thermostatpassword = ''
         this.thermostattesting = true 
         this.thermostatmethod = 'GET'
-        this.thermostatCommand = '?OID4.1.13='
+        this.thermostatTempCommand = 'OID4.1.13'
+        this.thermostatThermSetbackHeatCommand = 'OID4.1.5' 
+        this.thermostatThermHvacStateCommand = 'OID4.1.2'
+        this.thermostatGetCommand = `/get?${this.thermostatTempCommand}=&${this.thermostatThermSetbackHeatCommand}=&${this.thermostatThermHvacStateCommand}=` // = path in req
 
         // register a capability listener
         this.registerCapabilityListener('measure_temperature', this.onCapabilityMeasure_temperature.bind(this))
+        this.registerCapabilityListener('target_temperature', this.onCapabilityTarget_temperature.bind(this))
+        this.registerCapabilityListener('thermostat_mode', this.onCapabilityThermostat_mode.bind(this))
+
+
+        
 
         this.settings = this.getSettings();
 
@@ -60,7 +68,7 @@ class NT10Device extends Homey.Device {
 
         // test thermostat 
         if (!(this.ip == null) && !(this.port == null) && !(this.thermostatusername == null) && !(this.thermostatpassword == null)) {
-            this.req(this.ip, this.port, this.thermostatCommand, this.thermostatmethod, this.thermostatusername, this.thermostatpassword);
+            this.req(this.ip, this.port, this.thermostatGetCommand, this.thermostatmethod, this.thermostatusername, this.thermostatpassword);
             this.polleri += 1
         };
 
@@ -103,6 +111,56 @@ class NT10Device extends Homey.Device {
         return Promise.reject(new Error('Switching the device failed!'));
     }
 
+    onCapabilityTarget_temperature(value, opts, callback) {
+
+        // ... set value to real device
+        util.log(` onCapabilityTarget_temperature fired with value`, value)
+        util.log(` onCapabilityTarget_temperature fired with opts`, util.inspect(opts))
+
+        // return (5 / 9) * c + 32
+
+        let valueNumber = Number(value)
+
+           util.log(`valueNumber `, valueNumber)
+
+        let fahrenheid = (((( 9/5) * valueNumber + 32)).toFixed(0))*10
+
+        util.log(`fahrenheid `, fahrenheid)
+
+        let path = `/pdp?OID4.1.5=${fahrenheid}&submit=Submit`
+        let method = 'POST'
+
+        this.req(this.ip, this.port, path, method, this.thermostatusername, this.thermostatpassword)
+
+
+
+        // Then, emit a callback ( err, result )
+        callback(null);
+
+        // or, return a Promise
+        return Promise.reject(new Error('Switching the device failed!'));}
+
+
+
+
+
+    onCapabilityThermostat_mode(value, opts, callback) {
+
+        // ... set value to real device
+        util.log(` onCapabilityThermostat_mode fired with value`, value)
+        util.log(` onCapabilityThermostat_mode fired with opts`, util.inspect(opts))
+        // Then, emit a callback ( err, result )
+        callback(null);
+
+        // or, return a Promise
+        return Promise.reject(new Error('Switching the device failed!'));
+    }
+
+
+
+
+
+
 
 
     // if socket is reachable
@@ -118,7 +176,7 @@ class NT10Device extends Homey.Device {
 
             if (this.thermostatset && this.thermostatconnected) {
 
-                this.req(this.ip, this.port, this.thermostatCommand, this.thermostatmethod, this.thermostatusername, this.thermostatpassword);
+                this.req(this.ip, this.port, this.thermostatGetCommand, this.thermostatmethod, this.thermostatusername, this.thermostatpassword);
 
             };
             console.log('pollproliphix polleri after ', this.polleri);
@@ -190,25 +248,90 @@ class NT10Device extends Homey.Device {
                 //  this.serverdata = str;   // to send to driver
 
                 util.log(`result req`, result)
-                let is = result.indexOf(`=`)
-                let questionmark = result.indexOf(`?`)
-                let strippedresult = result.slice(is+1, questionmark)
-                util.log(`strippedresult req`, strippedresult)
-                let answer = parseInt(strippedresult)
-                util.log(`answer`, answer)
 
-                let temp = (((answer/10) - 32)/1.8).toFixed(1)
+                if (method == 'GET') {
+
+                    // average temp
+                    let is2 = result.indexOf(`ID4.1.13=`)
+                    let strippedresult2 = result.slice(is2 + 1 + 8, is2 + 12)
+                    util.log(`strippedresult2 req`, strippedresult2)
+                    let answer2 = parseInt(strippedresult2)
+                    util.log(`int strippedresult2 `, parseInt(strippedresult2))
+                    let temp = (((answer2 / 10) - 32) / 1.8).toFixed(2)
+                    util.log(`temperature`, `  ${temp}  Celsius`)
+                    this.setCapabilityValue(`measure_temperature`, Number(temp))
+
+
+                    // target temp
+                    let is3 = result.indexOf(`ID4.1.5=`)
+                    let strippedresult3 = result.slice(is3 + 1 + 7, is3 + 11)
+                    util.log(`strippedresult3 req`, strippedresult3)
+                    let answer3 = parseInt(strippedresult3)
+                    util.log(`int strippedresult3 `, parseInt(strippedresult3))
+                    let thermSetbackHeat = (((answer3 / 10) - 32) / 1.8).toFixed(2)
+                    util.log(`thermSetbackHeat`, ` ${thermSetbackHeat} `)
+                    this.setCapabilityValue(`target_temperature`, Number(thermSetbackHeat))
+
+
+
+
+                    // hvac state 
+                    let is4 = result.indexOf(`ID4.1.2=`)
+                    let strippedresult4 = result.slice(is4 + 1 + 7, is4 + 9)
+                    util.log(`strippedresult4 req`, strippedresult4)
+                    let answer4 = strippedresult4
+                    util.log(`int strippedresult4 `, strippedresult4)
+                    let thermHvacState = ``
+                    switch (answer4) {
+                        case "1":
+                            thermHvacState = `off`
+                            break;
+                        case "2":
+                            thermHvacState = `off`
+                            break;
+                        case "3":
+                            thermHvacState = `heat`
+                    }
+                    util.log(`thermHvacState`, ` ${thermHvacState} `)
+                    this.setCapabilityValue(`thermostat_mode`, thermHvacState)
+
+                } //if method
+                else if (method == 'POST') {
+                    let is3 = result.indexOf(`ID4.1.5=`)
+                    let strippedresult3 = result.slice(is3 + 1 + 7, is3 + 11)
+                    util.log(`strippedresult3 req`, strippedresult3)
+                    let answer3 = parseInt(strippedresult3)
+                    util.log(`int strippedresult3 `, parseInt(strippedresult3))
+                    let thermSetbackHeat = (((answer3 / 10) - 32) / 1.8).toFixed(2)
+                    util.log(`thermSetbackHeat set by Homey `, ` ${thermSetbackHeat} `)
+                    this.setCapabilityValue(`target_temperature`, Number(thermSetbackHeat))
+                } // else if 
+
+
+
+                //let is = result.indexOf(`=`)
+                //let questionmark = result.indexOf(`?`)
+                //let strippedresult = result.slice(is+1, questionmark)
+                //util.log(`strippedresult req`, strippedresult)
+                //let answer = parseInt(strippedresult)        
+                //let temp = (((answer/10) - 32)/1.8).toFixed(1)
               
-                util.log(`temperature`, `  ${temp}  Celsius`)
                 
-                this.setCapabilityValue(`measure_temperature`, Number(temp))
+                
+                
             }); // end on end
 
 
         };// callbCK2
 
+        //OID1.10.9=&OID1.2=&OID1.1=&OID1.4=&OID1.8=&OID2.7.1= 
+       // let path = `/get?${this.thermostatTempCommand}=&${this.thermostatThermSetbackHeatCommand}=&${this.thermostatThermHvacStateCommand}=`                               // /get?OID4.1.13=
+       // let path = '/get?OID4.1.13=&OID4.1.5=&OID4.1.2=&'
 
-        let path =  `/get${command}`                               // /get?OID4.1.13=
+       // this.thermostatCommand = '?OID4.1.13='
+      // let path = `/get${command}`  
+
+       let path = command
 
         let options2 = {
 
@@ -216,8 +339,11 @@ class NT10Device extends Homey.Device {
             port: port,
             path: path,
             method: method,
-            auth: username + ':' + password
-
+            auth: username + ':' + password,
+            timeout: 1000,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'                
+            }
         };
 
 
@@ -228,10 +354,10 @@ class NT10Device extends Homey.Device {
 
 
         console.log(' before req to be executed options2  ', options2);
-        var req = http.request(options2, callback2);
+        let req = http.request(options2, callback2);
         req.end();
-        req.on('error', function (err) {
-            console.log('err req', err)
+        req.on('error', (e) =>{
+            console.log('err req', e.message)
         });
 
     }// end request
